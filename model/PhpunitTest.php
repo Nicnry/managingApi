534cfacf88ec29ee673a80adeff82a85b52d2064
Nicolas Henry <nicolas.henry@cpnv.ch>
2020-03-05 11:32:38 +0100

    Create basic test for phpunit

---
+++ model/PhpunitTest.php
@@ -0,0 +1,11 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class PhpunitTest extends TestCase
+{
+    public function testPhpunit()
+    {
+        echo 'test working';
+        return true;
+    }
+}
