3832475d5aa62fb19b172926642b42e6b8320932
Nicolas Henry <nicolas.henry@cpnv.ch>
2020-03-06 09:29:21 +0100

    Create test file

--- model/GoogleApiTest.php
+++ model/GoogleApiTest.php
@@ -1,10 +1,11 @@
 <?php
 use PHPUnit\Framework\TestCase;
 
+require __DIR__.'/../model/GoogleApi.php';
 class GoogleApiTest extends TestCase
 {
     public function testGetLabels()
     {
-        
+        $google = new GoogleApi();
     }
 }
--- public/index.php
+++ public/index.php
@@ -1,60 +1,9 @@
 <?php
 
 require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/../model/GoogleApi.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/..');
 $dotenv->load();
 
-# Imports the Google Cloud client library
-use Google\Cloud\Storage\StorageClient;
-use Google\Cloud\Vision\V1\ImageAnnotatorClient;
-
-function auth_cloud_implicit($projectId)
-{
-    $config = [
-        'projectId' => $projectId,
-    ];
-    $storage = new StorageClient($config);
-}
-
-auth_cloud_implicit(getenv('PROJECT_ID'));
-
-/* function upload_object($bucketName, $objectName, $source)
-{
-    $storage = new StorageClient();
-    $file = fopen($source, 'r');
-    $bucket = $storage->bucket($bucketName);
-    $object = $bucket->upload($file, [
-        'name' => $objectName
-    ]);
-    printf('Uploaded %s to gs://%s/%s' . PHP_EOL, basename($source), $bucketName, $objectName);
-}
-
-upload_object(getenv('BUCKET_NAME'), 'name', 'path'); */
-
-// $path = 'path/to/your/image.jpg'
-
-function detect_label($path)
-{
-    $imageAnnotator = new ImageAnnotatorClient();
-
-    # annotate the image
-    $image = file_get_contents($path);
-    $response = $imageAnnotator->labelDetection($image);
-    $labels = $response->getLabelAnnotations();
-
-    if ($labels) {
-        print("Labels:" . PHP_EOL);
-        foreach ($labels as $label) {
-            print($label->getDescription() . PHP_EOL);
-            $labelJson[] = $label->serializeToJsonString();
-        }
-    } else {
-        print('No label found' . PHP_EOL);
-    }
-    echo json_encode($labelJson);
-
-    $imageAnnotator->close();
-}
-
-
-detect_label('./assets/saturnV.jpg');
+$google = new GoogleApi();
+echo $google->get_all_labels('./assets/saturnV.jpg');
