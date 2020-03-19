46650cf11ad367728fea327f97c3591dd376ed37
Alexandre Junod <alexandre.junod@cpnv.ch>
2020-03-19 14:52:51 +0100

    Update doc for test, and respect teacher architecture for test

--- model/GoogleLabelDetectorUnitTest.php
+++ model/GoogleLabelDetectorUnitTest.php
@@ -7,46 +7,64 @@ require __DIR__.'/../model/GoogleLabelDetectorImpl.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->load();
 
+/**
+ * This test class is designed to confirm the LabelDetector class's behavior
+ */
 class GoogleLabelDetectorUnitTest extends TestCase
 {
     private $labelDetector;
-    private $pathToTestFolder;
-    private $imageName;
-    private $jsonName;
     private $fullPathToImage;
     private $fullPathToExpectedJson;
-    private $maxLabels;
     private $imageUri;
     private $bucketUrl;
-    private $expectedJson;
+    private $imageName;
 
+    /**
+     * This test method initializes the context before each test method run.
+     *
+     * @return void
+     */
     protected function setUp(): void
     {
         //TODO: Use expected.json instead of our_expected.json, when the format will be fixed
-        $expectedJsonWithInvisibles = file_get_contents("./public/assets/our_expected.json");
-        $this->expectedJson = preg_replace('/\p{C}+/u', "", $expectedJsonWithInvisibles); // Remove invisible characters, they're given by file_get_contents 
         $this->labelDetector = new GoogleLabelDetectorImpl();
         $this->fullPathToImage = realpath('./public/assets/saturnV.jpg');
+        $this->fullPathToExpectedJson = realpath('./public/assets/our_expected.json');
     }
 
+    /**
+     * This test method checks the MakeAnalysis'answer when we try to analyze a local file
+     *
+     * @return void
+     */
     public function testMakeAnalysisLocalFileSuccess()
     {
         //given
         $actualJson = "";
+        $expectedJsonWithInvisibles = file_get_contents($this->fullPathToExpectedJson);
+        $expectedJson = preg_replace('/\p{C}+/u', "", $expectedJsonWithInvisibles); // Remove invisible characters, they're given by file_get_contents 
+        $this->imageUri = $this->fullPathToImage; // Used only to be more understandable
 
         //when
-        $this->labelDetector->MakeAnalysisRequest($this->fullPathToImage, $this->maxLabels);
+        $this->labelDetector->MakeAnalysisRequest($this->imageUri);
         
         //then
         //compare expected json with result json
         $actualJson = $this->labelDetector->ToString();
-        $this->assertEqualsIgnoringCase($this->expectedJson, $actualJson);
+        $this->assertEquals($expectedJson, $actualJson);
     }
 
+    /**
+     * This test method checks the MakeAnalysis'answer when we try to analyze a data object presents on a bucket
+     *
+     * @return void
+     */
     public function testMakeAnalysisBucketObjectSuccess()
     {
         //given
         $actualJson = "";
+        $expectedJsonWithInvisibles = file_get_contents($this->fullPathToExpectedJson);
+        $expectedJson = preg_replace('/\p{C}+/u', "", $expectedJsonWithInvisibles); // Remove invisible characters, they're given by file_get_contents 
         $this->imageUri = $this->bucketUrl + "/" + $this->imageName;
         
         //when
@@ -55,9 +73,14 @@ class GoogleLabelDetectorUnitTest extends TestCase
         //then
         //compare expected json with result json
         $actualJson = $this->labelDetector->ToString();
-        $this->assertEqualsIgnoringCase($this->expectedJson, $actualJson);
+        $this->assertEquals($expectedJson, $actualJson);
     }
 
+    /**
+     * This test method cleanups the context after each test method run.
+     *
+     * @return void
+     */
     protected function tearDown(): void
     {
 
