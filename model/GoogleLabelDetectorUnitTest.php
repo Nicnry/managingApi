1e8d0a90e3a3cbd310cec35360bdff004cccdb62
Nicolas Henry <nicolas.henry@cpnv.ch>
2020-03-19 11:09:59 +0100

    translate c# tests to php

--- model/GoogleLabelDetectorUnitTest.php
+++ model/GoogleLabelDetectorUnitTest.php
@@ -16,13 +16,15 @@ class GoogleLabelDetectorUnitTest extends TestCase
     private $fullPathToImage;
     private $fullPathToExpectedJson;
     private $maxLabels;
+    private $imageUri;
+    private $bucketUrl;
 
     protected function setUp(): void
     {
         //TODO: Use expected.json instead of our_expected.json, when the format will be fixed
         $expectedJson = file_get_contents("./public/assets/our_expected.json");
     }
-    
+
     public function testMakeAnalysisLocalFileSuccess()
     {
         //given
@@ -31,6 +33,22 @@ class GoogleLabelDetectorUnitTest extends TestCase
         
         //when
         $this->labelDetector->MakeAnalysisRequest($this->fullPathToImage, $this->maxLabels);
+        
+        //then
+        //compare expected json with result json
+        $actualJson = $this->labelDetector->ToString();
+        $this->assertEqualsIgnoringCase($expectedJson, $actualJson);
+    }
+
+    public function testMakeAnalysisBucketObjectSuccess()
+    {
+        //given
+        $actualJson = "";
+        $expectedJson = file_get_contents($this->fullPathToExpectedJson);
+        $this->imageUri = $this->bucketUrl + "/" + $this->imageName;
+        
+        //when
+        $this->labelDetector->MakeAnalysisRequest($this->imageUri);
         
         //then
         //compare expected json with result json
